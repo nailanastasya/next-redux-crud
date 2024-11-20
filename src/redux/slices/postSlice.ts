839eb28593b2d74601be95e6685b266d08460ebb
nailanastasya <nailah4nastasya.28@gmail.com>
2024-11-20 13:08:47 +0700

    first commit

--- src/redux/slices/postSlice.ts
+++ src/redux/slices/postSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: any = [
+// Initial State
+const initialState: any[] = [
   { id: 1, title: "Post 1", description: "Description of Post 1" },
   { id: 2, title: "Post 2", description: "Description of Post 2" },
   { id: 3, title: "Post 3", description: "Description of Post 3" },
@@ -8,12 +9,17 @@ const initialState: any = [
 
 const postsSlice = createSlice({
   name: "posts",
-  initialState: initialState,
+  initialState,
   reducers: {
-    addPost: () => {
-      // Implementasi logika untuk menambahkan post
+    // Menambahkan post baru
+    addPost: (state, action: PayloadAction<{ title: string; description: string }>) => {
+      const newPost = action.payload;
+      const newId = state.length > 0 ? state[state.length - 1].id + 1 : 1; // Pastikan ID unik
+      state.push({ id: newId, ...newPost }); // Tambahkan ke array
     },
-    updatePost: (state, action: PayloadAction<any>) => {
+
+    // Mengupdate post yang ada berdasarkan ID
+    updatePost: (state, action: PayloadAction<{ id: number; title: string; description: string }>) => {
       const { id, title, description } = action.payload;
       const postIndex = state.findIndex((post: any) => post.id === id);
       if (postIndex !== -1) {
@@ -21,16 +27,22 @@ const postsSlice = createSlice({
         state[postIndex].description = description;
       }
     },
-    deletePost: () => {
-      // Implementasi logika untuk menghapus post
+
+    // Menghapus post berdasarkan ID
+    deletePost: (state, action: PayloadAction<number>) => {
+      const postId = action.payload;
+      const index = state.findIndex((post: any) => post.id === postId);
+      if (index !== -1) {
+        state.splice(index, 1); // Menghapus item berdasarkan index
+      }
     },
   },
 });
 
+// Ekspor Reducers dan Selector
 export const { addPost, updatePost, deletePost } = postsSlice.actions;
 
 export const selectPostById = (state: any, postId: number) =>
   state.posts.find((post: any) => post.id === postId);
 
 export default postsSlice.reducer;
-
